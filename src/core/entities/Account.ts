83bb10d8fde458a5449a56a24400b720dd67bb99
Jonatas Lizandro <jonatas.exe@gmail.com>
2021-05-14 13:06:22 -0300

    create account entity

---
+++ src/core/entities/Account.ts
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export default class Account {
+  id: string;
+
+  owner: string;
+
+  balance: number;
+
+  dailyWithdrawLimit: number;
+
+  active: boolean;
+
+  type: number;
+
+  createdAt: Date;
+
+  constructor(owner: string, balance: number, dailyWithdrawLimit: number, active: boolean, type: number) {
+    this.id = uuidv4();
+    this.owner = owner;
+    this.balance = balance;
+    this.dailyWithdrawLimit = dailyWithdrawLimit;
+    this.active = active;
+    this.type = type;
+    this.createdAt = new Date();
+  }
+}
